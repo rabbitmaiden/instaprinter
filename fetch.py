a019a7aa105c98e6a53fcfce848f1c0e8c41ebde
Alice Lee <alice@raspberrypi.(none)>
2014-12-30 15:28:13 -0800

    Move startup to fetch

--- fetch.py
+++ fetch.py
@@ -184,7 +184,7 @@ def reprint():
 def run():
   try:
     main()
-  except KeyboardInterrupt:
+  except (KeyboardInterrupt, EOFError):
     interrupt()
   
 
@@ -206,10 +206,58 @@ def interrupt():
     interrupt()
     return 0
 
+def startup():
+  #just in case
+  os.chdir('/home/pi/instaprinter')
+
+  uname = subprocess.check_output(['uname']).strip()
+  rpi = (uname == 'Linux')
+  if rpi:
+      print (colored("Hello from the Raspberry Pi!!", "yellow"))
+  else:
+      print (colored("Hello from a mac laptop", "yellow"))
+      print (colored("Set this up for yourself", "red"))
+      sys.exit(1)
+      
+
+  sys.stdout.write(colored("Do we have an IP address? ", 'green'))
+  ip = subprocess.check_output(['hostname', '-I']);
+  if not re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', ip):
+      print (colored("\nNo ip address, please fix and restart", 'red'))
+      sys.exit(1)
+
+  sys.stdout.write(colored("Yes!\n", "cyan"))
+  sys.stdout.write(colored("Do we have internet connectivity? ", 'green'))
+
+  r = requests.get('http://www.google.com/')
+  if r.status_code != 200:
+      print (colored("\nCould not connect to the internet", 'red'))
+      sys.exit(1)
+
+  sys.stdout.write(colored("Yes!\n", "cyan"))
+  sys.stdout.write(colored("Updating codebase: ", 'green'))
+
+  git = subprocess.call(["git", "pull", "origin", "master"])
+  if git != 0:
+      print (colored("\nCould not update codebase", 'red'))
+      sys.exit(1)
+
+  sys.stdout.write(colored("Success!\n", 'cyan'))
+  sys.stdout.write(colored("Do we have a printer? ", "green"))
+
+  lpinfo = subprocess.check_output(['lpinfo', '--include-schemes', 'usb', '-v'])
+  if lpinfo == '':
+      print (colored("\nCould not find the printer", 'red'))
+      sys.exit(1)
+
+  sys.stdout.write(colored("Yes!\n", "cyan"))
+
+
+
 if __name__ == "__main__":
   try:
+    startup()
     run()
-  except KeyboardInterrupt:
+  except (KeyboardInterrupt, EOFError):
     pass
-  except EOFError:
-    pass
+
